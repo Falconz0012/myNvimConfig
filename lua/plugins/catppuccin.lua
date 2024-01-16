03a1ac51562c5ce59bd3703905e3e096ca71f5fe
Falconz0012 <tomasfalcao.26@gmail.com>
2024-01-16 18:12:49 +0000

    added lsp features and dashboard

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,9 @@
-return   {  
+return {
 	"catppuccin/nvim",
-  lazy = false,
+	lazy = false,
 	name = "catppuccin",
 	priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
-  
+	config = function()
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+        auto_installed = true,
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+				highlight = { enable = true },
 
-          indent = { enable = true }  
-})
-end
-  }
+				indent = { enable = true },
+			})
+		end,
+	},
 }
