40ca951ef4fdaac43315b6de9d2415cc06b864c4
Falconz0012 <tomasfalcao.26@gmail.com>
2024-09-08 19:19:29 +0000

    updated LSP and completions

---
+++ lua/plugins/dashboard-nvim.lua
@@ -0,0 +1,63 @@
+return {
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+
+	config = function()
+		local status_ok, alpha = pcall(require, "alpha")
+		if not status_ok then
+			return
+		end
+
+		local dashboard = require("alpha.themes.dashboard")
+		dashboard.section.header.val = {
+
+			[[                     ▀████▀▄▄              ▄█ ]],
+			[[                       █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
+			[[               ▄        █          ▀▀▀▀▄  ▄▀  ]],
+			[[              ▄▀ ▀▄      ▀▄              ▀▄▀  ]],
+			[[             ▄▀    █     █▀   ▄█▀▄      ▄█    ]],
+			[[             ▀▄     ▀▄  █     ▀██▀     ██▄█   ]],
+			[[              ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
+			[[               █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
+			[[              █   █  █      ▄▄           ▄▀   ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button("f", "   Find file", ":Telescope find_files <cr>"),
+			dashboard.button("e", "  New file", ":ene <bar> startinsert <cr>"),
+			dashboard.button("r", "   Recently used files", ":Telescope oldfiles <cr>"),
+			dashboard.button("t", "  Find text", ":Telescope live_grep <cr>"),
+			dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<cr>"),
+			dashboard.button("q", "   Quit neovim", ":qa<cr>"),
+		}
+
+		local function footer()
+			return "Don't stop until you are proud..."
+		end
+
+		dashboard.section.footer.val = footer()
+
+		dashboard.section.footer.opts.hl = "type"
+		dashboard.section.header.opts.hl = "include"
+		dashboard.section.buttons.opts.hl = "keyword"
+
+		dashboard.opts.opts.noautocmd = true
+		alpha.setup(dashboard.opts)
+	end,
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+}
